0b221f509fb0742985ceb12c5d63ac068051bd67
Evgeniy_Utkin <eugene.utkin@gmail.com>
2020-09-05 11:24:28 +0500

    articles#edit and #update implemented

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,8 +14,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    if @article.valid?
-      @article.save
+    if @article.save
       redirect_to @article
     else
       render action: 'new'
@@ -23,6 +22,16 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render action: 'edit'
+    end
 
   end
 
